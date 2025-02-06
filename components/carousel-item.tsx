b17abf58aa2f5a51b0be32426d9622e58bea63ef
Jeron <jeronokkers7@gmail.com>
2025-02-06 23:52:11 +0200

    Initial commit

--- components/carousel-item.tsx
+++ components/carousel-item.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useContext, useCallback, JSX } from "react";
-import { EmblaCarouselType } from "embla-carousel-react"; // Import the type
+import type { EmblaCarouselType } from "embla-carousel";
 import { CarouselContext } from "./carousel";
 import styles from "@/styles/carousel.module.css";
 
@@ -10,12 +10,16 @@ interface Props {
 }
 
 const CarouselItem: React.FC<Props> = ({ index, children }) => {
-  const { embla: emblaApi, selectedIndex } = useContext(CarouselContext);
+  const { embla: emblaApi, selectedIndex } = useContext(CarouselContext) as {
+    embla: EmblaCarouselType | undefined;
+    selectedIndex: number;
+  };
+
   const isActive = selectedIndex === index;
 
   const handleClick = useCallback(() => {
     if (!emblaApi) return;
-    (emblaApi as EmblaCarouselType).scrollTo(index); // Correct typing
+    emblaApi.scrollTo(index);
   }, [emblaApi, index]);
 
   return (
